9876cad8856ab41bd9ae4be22b065ea1f8e45667
Klaudia Dilcher <klaudia.dilcher@gmail.com>
2019-01-23 20:55:05 +0100

    typo

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     #setup a logger
     logger = logging.getLogger('AtomicSensorSimulation')
     logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler('spam.log')
+    file_handler = logging.FileHandler('logs/AtomicSensorSimulation.log')
     file_handler.setLevel(logging.DEBUG)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.ERROR)
