9ec64669964c8b3de82e340b7fba9c05f906fdc4
Klaudia Dilcher <klaudia.dilcher@gmail.com>
2019-01-29 21:45:04 +0100

    adding example tests and CI

--- atomic_sensor_simulation/main.py
+++ atomic_sensor_simulation/main.py
@@ -5,18 +5,19 @@ import sys
 
 
 def main():
-    # setup a logger
-    logger = logging.getLogger('atomic_sensor_simulation')
-    logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler('logs/atomic_sensor_simulation.log')
-    file_handler.setLevel(logging.DEBUG)
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+    # # setup a logger
+    # logger = logging.getLogger('atomic_sensor_simulation')
+    # logger.setLevel(logging.DEBUG)
+    # file_handler = logging.FileHandler('logs/atomic_sensor_simulation.log')
+    # file_handler.setLevel(logging.DEBUG)
+    # console_handler = logging.StreamHandler()
+    # console_handler.setLevel(logging.ERROR)
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # file_handler.setFormatter(formatter)
+    # console_handler.setFormatter(formatter)
+    # logger.addHandler(file_handler)
+    # logger.addHandler(console_handler)
+    print('shit')
 
     return 0
 
