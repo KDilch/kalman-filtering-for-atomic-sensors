6c3b589c82ffefb052cd8cb40f9096c6c7dec522
Klaudia Dilcher <klaudia.dilcher@gmail.com>
2019-01-30 11:49:50 +0100

    Wrapper for logging

--- atomic_sensor_simulation/main.py
+++ atomic_sensor_simulation/main.py
@@ -1,24 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import logging
-import sys
+from logger import Logger
 
 
 def main():
-    # # setup a logger
-    # logger = logging.getLogger('atomic_sensor_simulation')
-    # logger.setLevel(logging.DEBUG)
-    # file_handler = logging.FileHandler('logs/atomic_sensor_simulation.log')
-    # file_handler.setLevel(logging.DEBUG)
-    # console_handler = logging.StreamHandler()
-    # console_handler.setLevel(logging.ERROR)
-    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    # file_handler.setFormatter(formatter)
-    # console_handler.setFormatter(formatter)
-    # logger.addHandler(file_handler)
-    # logger.addHandler(console_handler)
-    print('shit')
-
+    # setup a logger
+    logger = Logger('atomic_sensor_simulation', log_file_path='logs/atomic_sensor_simulation.log')
+    logger.logger.info('Starting execution of Atomic Sensor Simulation.')
     return 0
 
 
