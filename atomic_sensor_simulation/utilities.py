7ff6732de7af818ed92020711fe1bac6252c0f20
Klaudia Dilcher <klaudia.dilcher@gmail.com>
2019-02-04 19:23:49 +0100

    resolves #5

--- atomic_sensor_simulation/utilities.py
+++ atomic_sensor_simulation/utilities.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 
-def stringify_namespace(argv):
+def stringify_namespace(namespace):
     """
     :param argv: namespace
     :return: string
     """
     __str = ''
-    for arg in argv.__dict__:
+    for arg in namespace.__dict__:
         if arg:
-            __str += " --" + arg + ": " + str(argv.__dict__[arg])
+            __str += " --" + arg + ": " + str(namespace.__dict__[arg])
         else:
             pass
     return __str
