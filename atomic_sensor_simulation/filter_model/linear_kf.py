Fixing Q_delta computation

atomic_sensor_simulation/filter_model/linear_kf.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
-from scipy.linalg import expm
+from numpy.linalg import matrix_power
+from scipy.integrate import simps
+from sympy import *
+from scipy.linalg import expm, solve_discrete_are
 from filterpy.kalman import KalmanFilter
 from scipy.integrate import odeint
 
@@ -45,7 +48,7 @@ class Linear_KF(Model):
                        dt=dt)
 
         self.Phi_delta = self.compute_Phi_delta_exp_Fdt_approx(from_time=0)
-        self.Q_delta = np.dot(np.dot(self.Phi_delta, self.Q), self.Phi_delta.transpose()) * dt
+        self.Q_delta = self.compute_Q_delta_sympy(from_time=0., Phi_0=self.Phi_delta)
         if any(x0) is None or P0 is None:
             self.x0, self.P0 = self.calculate_x0_and_P0(z0)
             self._logger.info('Setting default values for x0 and P0...')
@@ -54,6 +57,46 @@ class Linear_KF(Model):
             self.P0 = P0
         self.dim_x = len(self.x0)
 
+    def compute_Q_delta_sympy(self, from_time, Phi_0, num_terms=30):
+        def dPhidt(Phi, t):
+            return np.reshape(np.dot(np.array(eval_matrix_of_functions(self._F, t), dtype=float),
+                                     np.reshape(Phi, (4, 4))), 16)
+
+        t = np.linspace(from_time, from_time + self.dt, num=num_terms)  # times to report solution
+        Phi_deltas, _ = odeint(dPhidt, np.reshape(Phi_0, 16), t, full_output=True)
+        #NUMerical
+        Phi_s_matrix_form = [np.reshape(Phi_deltas[i], (4, 4)) for i in range(len(Phi_deltas))]
+        Phi_s_transpose_matrix_form = [np.transpose(a) for a in Phi_s_matrix_form]
+        integrands = np.array([np.dot(np.dot(a, self.Q), b) for a, b in zip(Phi_s_matrix_form, Phi_s_transpose_matrix_form)])
+        #Assuming 4x4 matrices!
+        a = integrands.reshape(-1, integrands.shape[-1])
+        int00, int01, int02, int03, int10, int11, int12, int13, int20, int21, int22, int23, int30, int31, int32, int33 = map(list, zip(*integrands.reshape(*integrands.shape[:1], -1)))
+        integrand_split = [int00, int01, int02, int03, int10, int11, int12, int13, int20, int21, int22, int23, int30, int31, int32, int33]
+        #calculate integral numerically using simsons rule
+        # integrands_flat = np.array(integrands).flatten()
+        # times_flat = np.repeat(t, integrands[0].shape[0]*integrands[0].shape[1])
+        # print(np.reshape(simps(integrands_flat, times_flat), (4, 4)))
+        # return np.reshape(simps(integrands_flat, times_flat), (4, 4))
+        self.Q_delta = np.reshape(np.array([simps(i, t) for i in integrand_split]), (4, 4))
+        # print('Q_delta', np.reshape(np.array([simps(i, t) for i in integrand_split]), (4, 4)))
+        return np.reshape(np.array([simps(i, t) for i in integrand_split]), (4, 4))
+        # #SYMPY
+        # out = zeros(*(self.Phi_delta.shape))
+        # for n in range(num_terms):
+        #     matrix_to_n = matrix_power(self.Phi_delta, n) / factorial(n)
+        #     Phi_Q_Phi_t_Nth_term = np.dot(np.dot(matrix_to_n, self.Q), np.transpose(matrix_to_n))
+        #     matrix_flat = Phi_Q_Phi_t_Nth_term.flatten()
+        #     shape = np.shape(Phi_Q_Phi_t_Nth_term)
+        #     matrix = np.empty_like(matrix_flat)
+        #     for index, element in np.ndenumerate(matrix_flat):
+        #         matrix[index] = lambda t: t ** (2 * n) * element
+        #     Phi_Q_PHI_T = np.reshape(matrix, shape)
+        #     from utilities import integrate_matrix_of_functions
+        #     int = integrate_matrix_of_functions(Phi_Q_PHI_T, from_x=from_time, to_x=from_time+self.dt)
+        #     Phi_delta_RF = expm(self.Phi_delta * self.dt)
+        #     out += np.dot(np.dot(Phi_delta_RF, int), np.transpose(Phi_delta_RF))
+        #     print(np.array(out).astype(np.float64))
+        # return np.array(out).astype(np.float64)
 
     def compute_Phi_delta_exp_Fdt_approx(self, from_time):
         """Returns solution for Phi from t_k to t_k+dt_filter as if F did not depend on time. Can be used for very slowly varying functions etc.

atomic_sensor_simulation/state/atomic_state.py
@@ -5,7 +5,8 @@ from enum import Enum
 
 from atomic_sensor_simulation.state.state import State
 
-from atomic_sensor_simulation.operable_functions import create_operable_cos_func, create_operable_const_func, create_operable_sin_func
+from atomic_sensor_simulation.operable_functions import create_operable_cos_func, create_operable_const_func, \
+    create_operable_sin_func, create_operable_step_func
 
 
 class AtomicSensorCoordinates(Enum):
@@ -45,36 +46,11 @@ class AtomicSensorState(State):
                                          create_operable_const_func(0)],
 
                                         [create_operable_const_func(-self.__larmour_freq),
-                                          create_operable_const_func(-self.__spin_correlation_const),
-                                          create_operable_cos_func(amplitude=self.__coupling_amplitude,
-                                                                   omega=self.__coupling_freq,
-                                                                   phase_shift=self.__coupling_phase_shift),
-                                          create_operable_sin_func(amplitude=self.__coupling_amplitude,
-                                                                   omega=self.__coupling_freq,
-                                                                   phase_shift=self.__coupling_phase_shift)],
-
-                                        [create_operable_const_func(0),
-                                         create_operable_const_func(0),
-                                         create_operable_const_func(-self.__light_correlation_const),
-                                         create_operable_const_func(0)
-                                         ],
-
-                                        [create_operable_const_func(0),
-                                         create_operable_const_func(0),
-                                         create_operable_const_func(0),
-                                         create_operable_const_func(-self.__light_correlation_const)]])
-
-        x_JACOBIAN = np.array([[create_operable_const_func(-self.__spin_correlation_const),
-                                         create_operable_const_func(self.__larmour_freq),
-                                         create_operable_const_func(0),
-                                         create_operable_const_func(0)],
-
-                                        [create_operable_const_func(-self.__larmour_freq),
                                          create_operable_const_func(-self.__spin_correlation_const),
-                                         create_operable_sin_func(amplitude=-self.__coupling_amplitude*self.__coupling_freq,
+                                         create_operable_cos_func(amplitude=self.__coupling_amplitude,
                                                                   omega=self.__coupling_freq,
                                                                   phase_shift=self.__coupling_phase_shift),
-                                         create_operable_cos_func(amplitude=self.__coupling_amplitude+self.__coupling_freq,
+                                         create_operable_sin_func(amplitude=self.__coupling_amplitude,
                                                                   omega=self.__coupling_freq,
                                                                   phase_shift=self.__coupling_phase_shift)],
 
@@ -89,22 +65,59 @@ class AtomicSensorState(State):
                                          create_operable_const_func(0),
                                          create_operable_const_func(-self.__light_correlation_const)]])
 
+        x_JACOBIAN = np.array([[create_operable_const_func(-self.__spin_correlation_const),
+                                create_operable_const_func(self.__larmour_freq),
+                                create_operable_const_func(0),
+                                create_operable_const_func(0)],
+
+                               [create_operable_const_func(-self.__larmour_freq),
+                                create_operable_const_func(-self.__spin_correlation_const),
+                                create_operable_sin_func(amplitude=-self.__coupling_amplitude * self.__coupling_freq,
+                                                         omega=self.__coupling_freq,
+                                                         phase_shift=self.__coupling_phase_shift),
+                                create_operable_cos_func(amplitude=self.__coupling_amplitude + self.__coupling_freq,
+                                                         omega=self.__coupling_freq,
+                                                         phase_shift=self.__coupling_phase_shift)],
+
+                               [create_operable_const_func(0),
+                                create_operable_const_func(0),
+                                create_operable_const_func(-self.__light_correlation_const),
+                                create_operable_const_func(0)
+                                ],
+
+                               [create_operable_const_func(0),
+                                create_operable_const_func(0),
+                                create_operable_const_func(0),
+                                create_operable_const_func(-self.__light_correlation_const)]])
+
         State.__init__(self,
                        initial_vec,
                        noise_vec,
                        AtomicSensorCoordinates,
                        F_transition_matrix=F_transition_matrix,
-                       x_jacobian= x_JACOBIAN,
+                       x_jacobian=x_JACOBIAN,
                        dt=dt,
                        time=initial_time,
                        u_control_vec=np.array([create_operable_const_func(0.),
                                                create_operable_const_func(0.),
                                                create_operable_const_func(0.),
                                                create_operable_const_func(0.)]).T,
-                       Gamma_control_evolution_matrix=np.array([[create_operable_const_func(1.), create_operable_const_func(0.), create_operable_const_func(0.), create_operable_const_func(0.)],
-                                                                [create_operable_const_func(0.), create_operable_const_func(1.), create_operable_const_func(0.), create_operable_const_func(0.)],
-                                                                [create_operable_const_func(0.), create_operable_const_func(0.), create_operable_const_func(1.), create_operable_const_func(0.)],
-                                                                [create_operable_const_func(0.), create_operable_const_func(0.), create_operable_const_func(0.), create_operable_const_func(1.)]]),
+                       Gamma_control_evolution_matrix=np.array([[create_operable_const_func(1.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.)],
+                                                                [create_operable_const_func(0.),
+                                                                 create_operable_const_func(1.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.)],
+                                                                [create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(1.),
+                                                                 create_operable_const_func(0.)],
+                                                                [create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(1.)]]),
                        logger=logger)
 
     @property
@@ -128,12 +141,146 @@ class AtomicSensorState(State):
 
     @property
     def quadrature(self):
-        return [self._state_vec[self._coordinates.QUADRATURE_Q.value], self._state_vec[self._coordinates.QUADRATURE_P.value]]
+        return [self._state_vec[self._coordinates.QUADRATURE_Q.value],
+                self._state_vec[self._coordinates.QUADRATURE_P.value]]
 
     @property
     def spin_mean(self):
-        return [self._mean_state_vec[self._coordinates.SPIN_Y.value], self._mean_state_vec[self._coordinates.SPIN_Z.value]]
+        return [self._mean_state_vec[self._coordinates.SPIN_Y.value],
+                self._mean_state_vec[self._coordinates.SPIN_Z.value]]
 
     @property
     def quadrature_mean(self):
-        return [self._mean_state_vec[self._coordinates.QUADRATURE_Q.value], self._mean_state_vec[self._coordinates.QUADRATURE_P.value]]
+        return [self._mean_state_vec[self._coordinates.QUADRATURE_Q.value],
+                self._mean_state_vec[self._coordinates.QUADRATURE_P.value]]
+
+
+class SawQuadratureState(State):
+    """
+        Specialization of a state abstract class. Represents a state vector x_t_k = [j, q].
+        """
+
+    def __init__(self, initial_vec, noise_vec, initial_time, dt=1., logger=None, **kwargs):
+        """
+            :param initial_vec:
+            :param noise_vec:
+            :param initial_time: float; a member variable __time of class state is initialized to initial time
+            :param logger: an instance of logger.Logger; if not passed a new instance of a logger is initialized
+            :param kwargs: key word args specific to a given simulation;
+                           in this case they are: atoms_wiener_const, g_a_coupling_const
+            """
+        self.__time = initial_time
+        self.__light_correlation_const = kwargs['light_correlation_const']
+        self.__coupling_amplitude = kwargs['coupling_amplitude']
+        self.__coupling_freq = kwargs['coupling_freq']
+        self.__coupling_phase_shift = kwargs['coupling_phase_shift']
+        self.__larmour_freq = kwargs['larmour_freq']
+        self.__spin_correlation_const = kwargs['spin_correlation_const']
+
+        # F is a matrix with entries being functions. This wa can be integrated easily. Keep funcitons defined in "operable_functions"
+        F_transition_matrix = np.array([[create_operable_const_func(-self.__spin_correlation_const),
+                                         create_operable_const_func(self.__larmour_freq),
+                                         create_operable_const_func(0),
+                                         create_operable_const_func(0)],
+
+                                        [create_operable_const_func(-self.__larmour_freq),
+                                         create_operable_const_func(-self.__spin_correlation_const),
+                                         create_operable_step_func(self.__coupling_amplitude,
+                                                                   omega=self.__coupling_freq),
+                                         create_operable_const_func(0.)],
+
+                                        [create_operable_const_func(0),
+                                         create_operable_const_func(0),
+                                         create_operable_const_func(-self.__light_correlation_const),
+                                         create_operable_const_func(0)
+                                         ],
+
+                                        [create_operable_const_func(0),
+                                         create_operable_const_func(0),
+                                         create_operable_const_func(0),
+                                         create_operable_const_func(-self.__light_correlation_const)]])
+
+        x_JACOBIAN = np.array([[create_operable_const_func(-self.__spin_correlation_const),
+                                create_operable_const_func(self.__larmour_freq),
+                                create_operable_const_func(0),
+                                create_operable_const_func(0)],
+
+                               [create_operable_const_func(-self.__larmour_freq),
+                                create_operable_const_func(-self.__spin_correlation_const),
+                                create_operable_const_func(0),
+                                create_operable_const_func(0)],
+
+                               [create_operable_const_func(0),
+                                create_operable_const_func(0),
+                                create_operable_const_func(-self.__light_correlation_const),
+                                create_operable_const_func(0)
+                                ],
+
+                               [create_operable_const_func(0),
+                                create_operable_const_func(0),
+                                create_operable_const_func(0),
+                                create_operable_const_func(-self.__light_correlation_const)]])
+
+        State.__init__(self,
+                       initial_vec,
+                       noise_vec,
+                       AtomicSensorCoordinates,
+                       F_transition_matrix=F_transition_matrix,
+                       x_jacobian=x_JACOBIAN,
+                       dt=dt,
+                       time=initial_time,
+                       u_control_vec=np.array([create_operable_const_func(0.),
+                                               create_operable_const_func(0.),
+                                               create_operable_const_func(0.),
+                                               create_operable_const_func(0.)]).T,
+                       Gamma_control_evolution_matrix=np.array([[create_operable_const_func(1.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.)],
+                                                                [create_operable_const_func(0.),
+                                                                 create_operable_const_func(1.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.)],
+                                                                [create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(1.),
+                                                                 create_operable_const_func(0.)],
+                                                                [create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(0.),
+                                                                 create_operable_const_func(1.)]]),
+                       logger=logger)
+
+    @property
+    def state_vec(self):
+        """Returns a numpy array representing a state vector x=(spin, quadrature)."""
+        return self._state_vec
+
+    @property
+    def mean_state_vec(self):
+        """Returns a numpy array representing a state vector x without any noise."""
+        return np.array(self._mean_state_vec)
+
+    @property
+    def noise_vec(self):
+        """Returns a numpy array representing a noise vector [noise_j, noise_q]."""
+        return self._noise_vec
+
+    @property
+    def spin(self):
+        return [self._state_vec[self._coordinates.SPIN_Y.value], self._state_vec[self._coordinates.SPIN_Z.value]]
+
+    @property
+    def quadrature(self):
+        return [self._state_vec[self._coordinates.QUADRATURE_Q.value],
+                self._state_vec[self._coordinates.QUADRATURE_P.value]]
+
+    @property
+    def spin_mean(self):
+        return [self._mean_state_vec[self._coordinates.SPIN_Y.value],
+                self._mean_state_vec[self._coordinates.SPIN_Z.value]]
+
+    @property
+    def quadrature_mean(self):
+        return [self._mean_state_vec[self._coordinates.QUADRATURE_Q.value],
+                self._mean_state_vec[self._coordinates.QUADRATURE_P.value]]